c2f12e03479fbe91e95f27784c1425da10e4df9e
rikogunawan100 <rikogunawan100@gmail.com>
2024-11-08 09:08:53 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Riko',
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 69, 163, 240),
+        primaryColorLight: Colors.white,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(33, 150, 243, 1),
+        ),
+        useMaterial3: true,
+        textTheme: GoogleFonts.titilliumWebTextTheme(),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
