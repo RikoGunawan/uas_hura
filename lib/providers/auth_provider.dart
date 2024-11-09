5370f5462d9b836726afd0513c0caa1b22152f5a
Sulis <sulistiani3399@gmail.com>
2024-11-09 10:53:40 +0700

    Auth Provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthProvider extends ChangeNotifier{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _user;
+
+  User? get user => _user;
+
+  bool get isLoggedIn => _user != null;
+
+  Future<void> login({required String email, required String password}) async {
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      _user = credential.user;
+      //sampai disini user sudah login
+    } on FirebaseAuthException catch (error) {
+     
+      switch (error.code) {
+      //kita cukup melihat ccode errornya
+        case 'INVALID_LOGIN_CREDENTIALS':
+          throw 'Invalid login credentials.';
+        default:
+          rethrow;
+      }
+    }
+
+    notifyListeners();
+  }
+
+  //prosedur logoutnya sederhana ngehapus data user di aplikasi
+   Future<void> logout() async {
+    _user = null;
+
+    await _auth.signOut();
+  
+
+    notifyListeners();
+  }
+}
+
+
